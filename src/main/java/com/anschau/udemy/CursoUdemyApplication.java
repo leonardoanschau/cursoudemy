0059b7a40673d9433098ce607a64cb1e86951842
Leonardo Anschau <leonardoanschau@gmail.com>
2018-03-19 18:20:08 -0300

    Criando operaçãod de instanciação

--- src/main/java/com/anschau/udemy/CursoUdemyApplication.java
+++ src/main/java/com/anschau/udemy/CursoUdemyApplication.java
@@ -1,12 +1,32 @@
 package com.anschau.udemy;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoUdemyApplication {
+import com.anschau.udemy.domain.Categoria;
+import com.anschau.udemy.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoUdemyApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository cr;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUdemyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		cr.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
