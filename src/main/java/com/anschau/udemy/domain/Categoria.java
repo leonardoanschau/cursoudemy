dc0b000c847b92a5182716b4a1ad383f6c22ff05
Leonardo Anschau <leonardoanschau@gmail.com>
2018-03-19 17:36:52 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/anschau/udemy/domain/Categoria.java
+++ src/main/java/com/anschau/udemy/domain/Categoria.java
@@ -2,6 +2,12 @@ package com.anschau.udemy.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -18,6 +24,8 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	public Integer getId() {
 		return id;
 	}
