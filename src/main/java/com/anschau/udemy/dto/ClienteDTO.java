77f001666c3a9bb397570114999a2de927308f96
Leonardo Anschau <leonardoanschau@gmail.com>
2018-03-22 16:24:36 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/anschau/udemy/dto/ClienteDTO.java
+++ src/main/java/com/anschau/udemy/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.anschau.udemy.domain.Cliente;
+import com.anschau.udemy.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
